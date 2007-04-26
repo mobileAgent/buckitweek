8626b20d546ec4490f0437af821067e13bdfae6a
mike <mike@09c841f9-68a0-45ab-b5e7-a15e97627b52>
2007-04-26 03:06:25 +0000

    add backup tasks
    git-svn-id: file:///tmp/buckitweek/buckitweek/trunk@17 09c841f9-68a0-45ab-b5e7-a15e97627b52

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,6 +2,11 @@
 # (http://manuals.rubyonrails.com/read/book/17). It allows you to automate
 # (among other things) the deployment of your application.
 
+require 'yaml'
+
+# mongrel has some replacements for restart and spinner:
+require 'mongrel_cluster/recipes'
+
 # =============================================================================
 # REQUIRED VARIABLES
 # =============================================================================
@@ -58,9 +63,6 @@ set :mongrel_prefix, "/usr/local/bin"
 set :mongrel_start_port, 3300
 set :mongrel_servers, 3
 
-# mongrel has some replacements for restart and spinner:
-require 'mongrel_cluster/recipes'
-
 # =============================================================================
 # SSH OPTIONS
 # =============================================================================
@@ -109,15 +111,10 @@ end
 
 desc "After updating the code populate a new database.yml"
 task :after_update_code, :roles => :app do
-  require "yaml"
-  set :production_db_password, proc {Capistrano::CLI.password_prompt("Production db remote password:");}
-  buffer = YAML::load_file('config/database.example.yml');
+  buffer = YAML::load_file('config/database.yml');
   # purge unneeded configurations
   buffer.delete('test');
   buffer.delete('development');
-  
-  # Stuff the production, all expected to be there except password
-  buffer['production']['password'] = production_db_password
 
   put YAML::dump(buffer),"#{release_path}/config/database.yml",:mode=>0644
 
@@ -216,71 +213,21 @@ task :after_setup do
   # crontab_configuration_setup
 end
 
-desc <<DESC
-An imaginary backup task. (Execute the 'show_tasks' task to display all
-available tasks.)
-DESC
+desc "Backup the remote production database"
 task :backup, :roles => :db, :only => { :primary => true } do
-  # the on_rollback handler is only executed if this task is executed within
-  # a transaction (see below), AND it or a subsequent task fails.
-  on_rollback { delete "/tmp/dump.sql" }
-
-  run "mysqldump -u theuser -p thedatabase > /tmp/dump.sql" do |ch, stream, out|
-    ch.send_data "thepassword\n" if out =~ /^Enter password:/
+  filename = "#{application}.dump.#{Time.now.to_i}.sql.bz2"
+  file = "/tmp/#{filename}"
+  on_rollback { delete file }
+  db = YAML::load_file("config/database.yml")
+  run "mysqldump -u #{db['production']['username']} --password=#{db['production']['password']} #{db['production']['database']} | bzip2 -c > #{file}"  do |ch, stream, data|
+    puts data
   end
+  `mkdir -p #{File.dirname(__FILE__)}/../backups/`
+  get file, "backups/#{filename}"
+  delete file
 end
 
-# Tasks may take advantage of several different helper methods to interact
-# with the remote server(s). These are:
-#
-# * run(command, options={}, &block): execute the given command on all servers
-#   associated with the current task, in parallel. The block, if given, should
-#   accept three parameters: the communication channel, a symbol identifying the
-#   type of stream (:err or :out), and the data. The block is invoked for all
-#   output from the command, allowing you to inspect output and act
-#   accordingly.
-# * sudo(command, options={}, &block): same as run, but it executes the command
-#   via sudo.
-# * delete(path, options={}): deletes the given file or directory from all
-#   associated servers. If :recursive => true is given in the options, the
-#   delete uses "rm -rf" instead of "rm -f".
-# * put(buffer, path, options={}): creates or overwrites a file at "path" on
-#   all associated servers, populating it with the contents of "buffer". You
-#   can specify :mode as an integer value, which will be used to set the mode
-#   on the file.
-# * render(template, options={}) or render(options={}): renders the given
-#   template and returns a string. Alternatively, if the :template key is given,
-#   it will be treated as the contents of the template to render. Any other keys
-#   are treated as local variables, which are made available to the (ERb)
-#   template.
-
-desc "Demonstrates the various helper methods available to recipes."
-task :helper_demo do
-  # "setup" is a standard task which sets up the directory structure on the
-  # remote servers. It is a good idea to run the "setup" task at least once
-  # at the beginning of your app's lifetime (it is non-destructive).
-  setup
-
-  buffer = render("maintenance.rhtml", :deadline => ENV['UNTIL'])
-  put buffer, "#{shared_path}/system/maintenance.html", :mode => 0644
-  sudo "killall -USR1 dispatch.fcgi"
-  run "#{release_path}/script/spin"
-  delete "#{shared_path}/system/maintenance.html"
-end
-
-# You can use "transaction" to indicate that if any of the tasks within it fail,
-# all should be rolled back (for each task that specifies an on_rollback
-# handler).
-
-desc "A task demonstrating the use of transactions."
-task :long_deploy do
-  transaction do
-    update_code
-    disable_web
-    symlink
-    migrate
-  end
-
-  restart
-  enable_web
+desc "Backup the database before running migrations"
+task :before_migrate do 
+  backup
 end
